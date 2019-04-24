555cec19574c2b71806d0c7596f31460fbe42c32
matias chavez <matroxmega@gmail.com>
2019-04-24 18:51:09 -0400

    move table to product component

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IProduct } from 'src/app/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+@Input('datos') public products: IProduct[];
 
   constructor() { }
 
