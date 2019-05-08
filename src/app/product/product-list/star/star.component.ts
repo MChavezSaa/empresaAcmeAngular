c8d6358295c61069f730e172e0d6fc5703773a1e
matias chavez <matroxmega@gmail.com>
2019-05-08 18:46:13 -0400

    change static stars to dinamic stars

--- src/app/product/product-list/star/star.component.ts
+++ src/app/product/product-list/star/star.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent {
+export class StarComponent implements OnChanges{
 @Input() rating : number =100;
 stars: number= 4;
-arr: any = new Array (this.stars).fill(1);
+arr:any;
+
+ngOnChanges():void{
+    if(this.rating>0 && this.rating<=40){
+      this.stars=1;
+    }else if(this.rating>41 && this.rating<=80){
+      this.stars=2;
+    }else if(this.rating>81 && this.rating<=120){
+        this.stars=3;
+    }else if(this.rating>121 && this.rating<=160){
+      this.stars=4;
+    }else if(this.rating>161 && this.rating<=200){
+      this.stars=5;
+    }
+    this.arr= new Array (this.stars).fill(1); 
+  }
 }
